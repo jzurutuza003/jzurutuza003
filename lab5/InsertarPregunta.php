5e5339c604708dcb11d08a491fe036f62b554244
jzurutuza003 <jzurutuza003@gmail.com>
2015-11-02 04:50:04 +0100

    xml

--- lab5/InsertarPregunta.php
+++ lab5/InsertarPregunta.php
@@ -2,14 +2,21 @@
 session_start();
 if($_SESSION['logueado']=='1'){
 	if(isset($_POST['Pregunta'])){
+	try{	
 	$Correo=$_SESSION['correo'];
 	$Pregunta=$_POST['Pregunta'];
 	$Complejidad=$_POST['Valoracion'];
 	$Respuesta=$_POST['Respuesta'];
+	$Tematica=$_POST['Tematica'];
 	
 	$usu=mysql_connect("mysql.hostinger.es","u878461769_mysql","abcd1234");
-
-	mysql_select_db("u878461769_mysql",$usu);
+	if(!$usu){
+		throw new Exception($error);
+	}
+	
+	if(!mysql_select_db("u878461769_mysql",$usu)){
+		throw new Exception($error);
+	}
 	$buscarPregunta = "SELECT * FROM Quiz WHERE  Pregunta=$Pregunta "; 
 	$result = mysql_query($buscarPregunta); 
 	$count = mysql_num_rows($result); 
@@ -20,20 +27,44 @@ if($_SESSION['logueado']=='1'){
 	}else{
 		$tabla="SELECT * FROM Quiz";
 		$tabla2=mysql_query($tabla);
+		if(!$tabla2){throw new Exception($error);}
 		$Numero = mysql_num_rows($tabla2);
 		$Numero=$Numero +1;
-		$SQL1=" INSERT INTO Quiz VALUES('$Numero', '$Correo','$Pregunta','$Respuesta','$Complejidad')";
+		$SQL1=" INSERT INTO Quiz VALUES('$Numero', '$Correo','$Pregunta','$Respuesta','$Complejidad','$Tematica')";
 		if (!mysql_query($SQL1))
 		{
-		die('Error: ' . mysql_error());
+		throw new Exception($error);
 		}
 		}
 		$_SESSION["logueado"]=0;
 		mysql_close($usu);
+		$xml = simplexml_load_file('preguntas.xml');
+		$assessmentItem=$xml->addChild('assessmentItem');
+		$itemBody=$assessmentItem->addChild('itemBody');
+		$p=$itemBody->addChild('p',$_POST['Pregunta']);
+		
+		$correctResponse=$assessmentItem->addChild('correctResponse'); 
+		$value=$correctResponse->addChild('value',$_POST['Respuesta']); 
+		$assessmentItem->addAttribute('complexity',$_POST['Valoracion']);
+		$assessmentItem->addAttribute('subject',$_POST['Valoracion']);
+		
+		if (!$xml->asXML('preguntas.xml') )
+		{
+		throw new Exception($error);
+		}
 		echo '<script type="text/javascript"> 
 	alert("Se ha introducido la pregunta en la base de datos");
 	</script>';
-	header("Location:visualizarPreguntas.php");
+	
+	}catch(Exception $error){
+		 echo '<script language="javascript">alert("Pregunta no introducida");</script>'; 
+		
+		
+	}
+		 echo '<script language="javascript">alert("Pregunta introducida");</script>'; 
+		sleep(2);
+	header("Location:VisualizarXml.php");
+	
 		
 	}else{
 		?>
@@ -53,6 +84,7 @@ if($_SESSION['logueado']=='1'){
 				<p id='p1'>Pregunta:
 					<input type='text' id='Correo' name='Pregunta'></p>
 				<p id='p2'>Respuesta: <input type='text' id='pass'name='Respuesta'></p>
+				<p id='p2'>Temática: <input type='text' id='p'name='Tematica'></p>
 				<p id='p4'>Valoracion: <input type='radio' id='1'name='Valoracion' value=1>1
 									<input type='radio' id='2'name='Valoracion'value=2>2
 									<input type='radio' id='3'name='Valoracion'value=3>3
@@ -69,6 +101,7 @@ if($_SESSION['logueado']=='1'){
 	<?php	
 	}
 }else{
+	
 	header("Location:login.php");
 }
 ?>
